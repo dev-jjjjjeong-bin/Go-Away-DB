8b799cc6d5d6611eb97e6b063dc5ab2af0fb3250
dev-jjjjjeong-bin <86920724+dev-jjjjjeong-bin@users.noreply.github.com>
2024-03-27 16:39:13 +0900

    [REFACTOR] App.js

--- Frontend/App.js
+++ Frontend/App.js
@@ -3,9 +3,9 @@
 import React from 'react';
 import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
 import { Colors, DebugInstructions, Header, LearnMoreLinks, ReloadInstructions } from 'react-native/Libraries/NewAppScreen';
-import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoadingScreen from './src/screens/LoadingScreen';
 import InputInfo from './src/screens/InputInfo';
 import Home from './src/screens/Home';
@@ -16,27 +16,21 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Loading"
-          component={LoadingScreen}
-          options={{headerShown: false}}   // 로딩 화면에는 헤더가 보이지 않도록 설정
-        />
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false, // 모든 스크린에 대해 헤더를 숨김
+        }}
+      >
+        <Stack.Screen name="Loading" component={LoadingScreen} />
         <Stack.Screen
           name="InputInfo"
           component={InputInfo}
-          options={{headerShown: false, tabBarStyle: {display: 'none'}}}   // InputInfo 화면에도 헤더가 보이지 않도록 설정
+          options={{
+            tabBarStyle: { display: 'none' },
+          }}
         />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Challenge"
-          component={Challenge}
-          options={{ headerShown: false }}
-        />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Challenge" component={Challenge} />
       </Stack.Navigator>
     </NavigationContainer>
   );
