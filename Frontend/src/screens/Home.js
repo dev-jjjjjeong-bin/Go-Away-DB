751fb28d3c80267bb7f9861a77c5288f427dde9f
dev-jjjjjeong-bin <86920724+dev-jjjjjeong-bin@users.noreply.github.com>
2024-04-01 13:41:20 +0900

    [ADD] TodayExercise - AutoOrDirect Screen

--- Frontend/src/screens/Home.js
+++ Frontend/src/screens/Home.js
@@ -11,8 +11,11 @@ const Home = ({ navigation }) => {
   const gotoCameraScreen = () => {
     navigation.navigate('Camera');
   };
-  const gotoLevelScreen = () => {
-    navigation.navigate('Level');
+  const gotoAutoOrDirectScreen = () => {
+    navigation.navigate('AutoOrDirect');
+  };
+  const gotoCalendarScreen = () => {
+    navigation.navigate('Plan');
   };
 
   return (
@@ -31,16 +34,16 @@ const Home = ({ navigation }) => {
         </View>
         <Image source={require('../assets/images/ArrowIcon.png')} style={styles.arrow} />
       </TouchableOpacity>
-      <TouchableOpacity style={styles.box} onPress={gotoLevelScreen}>
+      <TouchableOpacity style={styles.box} onPress={gotoAutoOrDirectScreen}>
         <View style={styles.textBox}>
           <Text style={styles.boxText}>오늘의 운동</Text>
           <Text style={styles.descriptionText}>매일 운동 계획을 간편하게 자동으로{'\n'}세울 수 있어요</Text>
         </View>
         <Image source={require('../assets/images/ArrowIcon.png')} style={styles.arrow} />
       </TouchableOpacity>
-      <TouchableOpacity style={styles.box}>
+      <TouchableOpacity style={styles.box} onPress={gotoCalendarScreen}>
         <View style={styles.textBox}>
-          <Text style={styles.boxText}>캘린더 확인</Text>
+          <Text style={styles.boxText}>운동 기록</Text>
           <Text style={styles.descriptionText}>나의 운동 기록을 확인할 수 있어요</Text>
         </View>
         <Image source={require('../assets/images/ArrowIcon.png')} style={styles.arrow} />
