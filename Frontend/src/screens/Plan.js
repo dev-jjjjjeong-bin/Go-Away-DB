6c52a4767f268af51e17818a6717c440da4e7307
dabeen <ekqls09@hanyang.ac.kr>
2024-04-03 16:31:44 +0900

    Add Calendar Page - Add Part Work List

--- Frontend/src/screens/Plan.js
+++ Frontend/src/screens/Plan.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 
-import React, {useState} from 'react';
-import { Image, View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Image, View, Text, TouchableOpacity, StyleSheet, TextInput, SafeAreaView, ScrollView} from 'react-native';
 import { Calendar, LocaleConfig } from 'react-native-calendars'
 import BottomBar from "../components/BottomBar";
 
@@ -50,26 +50,73 @@ LocaleConfig.locales.fr = {
 LocaleConfig.defaultLocale = 'fr';
 
 const Plan = ({ navigation }) => {
+    // 차후 백데이터로부터 연결
+    const [list, setList] = useState(["2024-04-04", "2024-04-25", "2024-04-16", "2024-05-01"]); // 예시
+    const [bodyTexts, setBodyTexts] = useState({
+        어깨: ["어깨1","어깨2"],
+        등: [],
+        가슴: ["가슴1"],
+        복부: [],
+        하체: ["하체1"]
+    });
+    const [markedDates, setMarkedDates] = useState({});
+
+    // 백데이터 연결할 필요 x, 프론트에서만 처리
     const [selectedDay, setSelectedDay] = useState('');
+    const BodyParts = ['어깨', '등', '가슴', '복부', '하체'];
+    const [showTextInput, setShowTextInput] = useState({
+        어깨: false,
+        등: false,
+        가슴: false,
+        복부: false,
+        하체: false
+    });
+    const [textInput, setTextInput] = useState({
+        어깨: '',
+        등: '',
+        가슴: '',
+        복부: '',
+        하체: ''
+    });
+
+    const handleInputBtn = (part) => {
+        setShowTextInput(prevState => ({
+            ...prevState,
+            [part]: !prevState[part]
+        }));
+    };
+    const handleInputTxt = (part, inputText) => {
+        setTextInput(prevState => ({
+            ...prevState,
+            [part]: inputText
+        }))
+    }
+
+    useEffect(() => {
+        setMarkedDates(list.reduce((acc, current) => {
+            acc[current] = {selected: true, selectedColor: '#99CCFF', selectedTextColor:'#5B5B5B' };
+            return acc;
+        }, {}));
+    }, [list]);
+
 
     return (
-        <View style={styles.container}>
-            <View style={styles.calendarContainer}>
+        <SafeAreaView style={styles.container}>
+            <View id="calendar" style={styles.calendarContainer}>
                 <Calendar
                     style={styles.calendar}
                     monthFormat = {'yyyy.MM'}
-                    hideExtraDays={false}
+                    hideExtraDays={true}
                     onDayPress={day => {
                         setSelectedDay(day.dateString);
                     }}
                     markedDates={{
-                        [selectedDay]: {selected: true, disableTouchEvent: true, selectedDotColor: '#1047AD'},
+                        ...markedDates,
+                        [selectedDay]: {selected: true, disableTouchEvent: true, selectedColor: '#1047AD', textColor: 'white'}
                     }}
                     theme={{
-                        dayTextColor: '#8E8E8E',
-                        backgroundColor: '#FFFFFF',
+                        dayTextColor: '#5B5B5B',
                         selectedDayBackgroundColor: '#1047AD',
-                        selectedDayTextColor: '#FFFFFF',
                         textDayFontSize: 16,
                         textDayFontWeight: 'bold',
                         textMonthFontSize: 20,
@@ -89,14 +136,41 @@ const Plan = ({ navigation }) => {
                         />}
                 />
             </View>
-            <View style={styles.plannerContainer}>
+            <ScrollView style={styles.plannerContainer}>
                 <Text style={styles.todayText}>5월 29일</Text>
-                <TouchableOpacity style={styles.plannerBtn}>
-                    <Text style={styles.plannerTxt}>오늘의 운동 계획하기</Text>
-                </TouchableOpacity>
-            </View>
+                <View id="dayList" style={{paddingBottom:80}}>
+                    {BodyParts.map(part => (
+                        <View key={part}>
+                            <TouchableOpacity id="partListAddBtn" onPress={() => console.log(`${part} 추가하기 눌림`)} style={{marginBottom: 15}}>
+                                <View style={{flexDirection:"row", alignItems: 'center'}}>
+                                    <View id="partName" style={styles.partContainer}>
+                                        <Text style={styles.partText}>{part}</Text>
+                                    </View>
+                                    <Text style={{fontFamily: 'SCDream6', fontSize: 15}}>추가하기 +</Text>
+                                </View>
+                            </TouchableOpacity>
+                            {bodyTexts[part].map((list, index) => (
+                                <TouchableOpacity
+                                    key={list}
+                                    style={styles.listContainer}
+                                    //onPress={() => handleBoxPress(part, index)}
+                                >
+                                    <View id="checkBox" style={[styles.checkBox,{backgroundColor: list ? 'white': '#1047AD'}]}>
+                                        <Image
+                                            name="checkBox"
+                                            source={require('../assets/images/Check.png')}
+                                            style={{height:14, width:16}}
+                                        />
+                                    </View>
+                                    <Text id="listText" style={{fontFamily:"SCDream4", fontSize: 18, color: 'black'}}>{list}</Text>
+                                </TouchableOpacity>
+                            ))}
+                        </View>
+                    ))}
+                </View>
+            </ScrollView>
             <BottomBar />
-        </View>
+        </SafeAreaView>
     );
 };
 
@@ -118,28 +192,51 @@ const styles = StyleSheet.create({
     plannerContainer: {
         flex:1,
         backgroundColor: '#EAEAEA',
-        height: 320,
-        paddingLeft: 25,
-        paddingRight: 25,
+        paddingHorizontal: 25,
         paddingTop: 20,
     },
     todayText: {
         fontFamily: 'SCDream7',
         fontSize: 15,
-        color: 'black'
+        color: 'black',
+        marginBottom: 10
     },
-    plannerBtn: {
-        marginTop: 22,
-        backgroundColor: '#1047AD',
-        height: 50,
+    partContainer: {
         borderRadius: 10,
+        height: 28,
+        width: 50,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        backgroundColor: '#99CCFF',
+        marginRight: 10
     },
-    plannerTxt: {
-        fontFamily: 'SCDream4',
+    partText: {
+        fontFamily: 'SCDream5',
         fontSize: 16,
-        color: 'white',
+        color: 'white'
+    },
+    listContainer: {
+        flexDirection: 'row',
+        backgroundColor: 'white',
+        marginBottom: 15,
+        height: 46,
+        borderRadius: 10,
+        alignItems: 'center',
+    },
+    checkBox: {
+        height:20,
+        width: 20,
+        borderWidth: 2,
+        borderColor: '#1047AD',
+        borderRadius: 3,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 24,
+        marginRight: 10,
+    },
+    textInput: {
+        height: 46,
+
     }
 });
 
