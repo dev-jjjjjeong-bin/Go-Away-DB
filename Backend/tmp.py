e6a757d70c58c7ffebda909f9f122241fc3250e0
chltndus601 <chltndus601@gmail.com>
2024-04-05 15:55:44 +0900

    240405 classfication 수정

--- Backend/tmp.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('python.db')
-cursor = conn.cursor()
-cursor.execute("select id from member")
-rows = cursor.fetchall()
-conn.close()
-print(int(rows[0][0]))
