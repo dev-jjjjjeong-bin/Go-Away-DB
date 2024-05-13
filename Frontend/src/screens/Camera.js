68d0a525f72e50d122b9cc60143c38bc392f0ba2
dabeen <ekqls09@hanyang.ac.kr>
2024-05-13 15:34:24 +0900

    ADD Uploading function

--- Frontend/src/screens/Camera.js
+++ Frontend/src/screens/Camera.js
@@ -1,21 +1,61 @@
 /* eslint-disable */
 
-import React, { useEffect } from 'react';
-import { Image, StyleSheet, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { Image, TouchableOpacity, StyleSheet, View, Text, ImageURISource } from 'react-native';
 import BottomBar from '../components/BottomBar.js';
 import LogoLocation from '../components/LogoLocation.js';
+import {launchImageLibrary} from 'react-native-image-picker';
 
 const Camera = ({ navigation }) => {
+  const UploadImage = async () => {
+    let image = null;
+
+    await launchImageLibrary({ mediaType: 'photo' }, (response) => {
+      if (response.didCancel) {
+        console.log("User cancelled image picker");
+      } else if (response.errorCode) {
+        console.log("ImagePicker ERROR :", response.errorCode);
+      } else if (response.assets) {
+        image = response.assets[0];
+      }
+    });
+
+    if (!image) return; // Exit if no image is selected
+
+    const formData = new FormData();
+    formData.append('file', {
+      uri: image.uri,
+      type: image.type || 'image/jpeg',  // Default to JPEG if type isn't provided
+      name: image.fileName || 'temp_image.jpg',  // Default name if not provided
+    });
+
+    try {
+      const response = await fetch('http://43.201.96.95:80/search', {
+        method: 'POST',
+        body: formData,
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      if (!response.ok) throw new Error(`Server responded with status ${response.status}`);
+      const result = await response.json();
+      console.log("SUCCESS:", result);
+    } catch (error) {
+      console.error('ERROR:', error);
+    }
+  };
+
 
   return (
     <View style={styles.container}>
       <LogoLocation />
       <View style={styles.contentContainer}>
         <Text style={styles.text}>카메라 영역에 운동기구가 {'\n'}잘리지 않도록 촬영해주세요</Text>
-        <View style={styles.greyBox}>
+        <TouchableOpacity style={styles.greyBox} onPress={UploadImage}>
           <Image source={require('../assets/images/CameraIcon.png')} style={styles.icon} />
           <Text style={styles.buttonText}>촬영하기</Text>
-        </View>
+        </TouchableOpacity>
       </View>
       <BottomBar style={styles.bottomBar} />
     </View>
